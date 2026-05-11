c4c066bb410a5c07ae042fbb40b16cf83cad540c
neekunj45-arch <neekunj45@gmail.com>
2026-05-11 10:33:30 +0530

    StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,8 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems,setCartItems] = useState({});
-    const url = "http://localhost:4000"
+    // const url = "http://localhost:4000"
+    const url = "https://online-shoes-shopping-backend.onrender.com"
     const [token,setToken] = useState("");
     const [product_list,setProductList] = useState([])
 
